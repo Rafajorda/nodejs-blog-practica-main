2c7a06bcdf67eb882e4b4e856a4fbfbcdcb18d5a
Rafajorda <rafajorgis@gmail.com>
2025-01-09 16:48:47 +0100

    badge-job2

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-Example of nextjs project using Cypress.io
+# Example of nextjs project using Cypress.io
 
 <!---Start place for the badge -->
 [![Cypress.io](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)](https://www.cypress.io/)
-
 <!---End place for the badge -->
+
+RESULTADOS DE LOS ÚLTIMOS TESTS
+![Resultado de los tests de Cypress](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)
---
+++ actions/add-badge/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const core = require('@actions/core');
+const path = require('path');
+
+const outcome = core.getInput('outcome');
+
+let badgeUrl;
+if (outcome === 'success') {
+  badgeUrl = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg';
+} else {
+  badgeUrl = 'https://img.shields.io/badge/test-failure-red';
+}
+
+const badgeText = `![Cypress Test Result](${badgeUrl})`;
+
+const readmePath = path.join(process.cwd(), 'README.md');
+
+fs.readFile(readmePath, 'utf8', (err, data) => {
+  if (err) {
+    core.setFailed(`Error leyendo el archivo README.md: ${err.message}`);
+    return;
+  }
+
+  const regex =/RESULTADOS DE LOS ÚLTIMOS TESTS/;
+  if (regex.test(data)) {
+    const updatedData = data.replace(regex, `RESULTAT DELS ÚLTIMS TESTS\n\n${badgeText}`);
+
+    fs.writeFile(readmePath, updatedData, 'utf8', (writeErr) => {
+      if (writeErr) {
+        core.setFailed(`Error escribiendo el archivo README.md: ${writeErr.message}`);
+      } else {
+        core.info('El README.md se ha actualizado correctamente con el badge.');
+      }
+    });
+  } else {
+    core.setFailed('No se encontró la sección "RESULTAT DELS ÚLTIMS TESTS" en el README.md.');
+  }
+});
